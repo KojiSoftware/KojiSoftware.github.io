3b58efef00eb6d5f66ddecf604abb41bc8a39b19
Maysun Campos <maysuncampos@yahoo.com>
2022-12-06 14:25:11 -0700

    Create lesson.js

---
+++ learn/js/lesson.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  document.getElementById("lesson").innerHTML = "<h1>Learn JavaScript</h1><div id='comp1'></div>";
+  document.getElementById("comp1").innerHTML = "<big>Basic Info</big><div id='info'></div>";
+  document.getElementById("info").innerHTML = "<p>in this part of the lesson you will learn how to do the basics<br/>logging, windows, & variables</p><div id='comp2'></div>";
+  document.getElementById("comp2").innerHTML = "<button onclick='part1()'>Start Coding</button>";
+}
+        
+function part1() {
+ document.getElementById("lesson").innerHTML = "<h1>JavaScript Logging</h1><div id='comp3'></div>";
+ document.getElementById("comp3").innerHTML = "How to log to the console, this is to print something to the terminal try so how you do this is you type <b>console.log('your message');</b><div id='comp4'></div>";
+ document.getElementById("comp4").innerHTML = "<button onclick='trypart1()'>Try it</button>";
+}
+      
+function trypart1() {
+}
