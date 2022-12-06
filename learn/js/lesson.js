3aa96c57237008f47f40974f9ed322d9d6dd709b
Maysun Campos <maysuncampos@yahoo.com>
2022-12-06 13:56:57 -0700

    Delete lesson.js

--- learn/js/lesson.js
+++
@@ -1,5 +0,0 @@
-window.onload = function()
-{
-  document.getElementById("lesson").innerHTML = "<h1>Learn JavaScript</h1><div id='comp1'></div>";
-  document.getElementById("comp1").innerHTML = "";
-}
