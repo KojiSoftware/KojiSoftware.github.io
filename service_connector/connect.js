ca026f5e5d24e93e5090b023c04ee78d1b1aaaf3
Maysun Campos <maysuncampos@yahoo.com>
2022-12-06 20:48:24 -0700

    Delete service_connector directory

--- service_connector/connect.js
+++
@@ -1,4 +0,0 @@
-window.onload=function(){document.body.innerHTML="<h1>Connect To Our Servers Via Github &amp; Alts</h1><div id='componentB'></div>";
-document.getElementById("componentB").innerHTML="<button onclick='github()'>Use Github</button><br/><button onclick='connect()'>Connect via JS</button>";}
-function github(){window.open("http://github.com/kojisoftware/kojisoftware.github.io/");}
-/* Copy this code */
